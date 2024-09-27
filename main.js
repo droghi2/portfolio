87157097d8d57e2290791f92aa2937067c63a280
Matei Bordei <themaftei@gmail.com>
2024-09-27 17:12:32 +0300

    edit project

--- main.js
+++ main.js
@@ -40,6 +40,10 @@ let projects = [
     image: 'textures/project-wholesale.jpg',
     url: 'https://wholesale.com.np/',
   },
+  {
+    image: 'textures/CaveEntrance.png',
+    url: 'https://wholesale.com.np/',
+  },
 ];
 let aboutCameraPos = {
   x: 0.12,
